b65d34c81877fbcf88286ad7a975a895778a0da9
H. Yalman <halil@yalman.io>
2025-05-22 11:32:52 +0200

    Korrekturen

--- src/main/java/org/example/report/BenchmarkResult.java
+++ src/main/java/org/example/report/BenchmarkResult.java
@@ -7,51 +7,94 @@ import java.util.LinkedHashMap;
  * DTO für die Benchmark-Ergebnisse inkl. abgeleiteter Metriken.
  */
 public class BenchmarkResult {
-    private String name;
-    private int bitSize;
-    private double score;           // ops/s
-    private double scoreError;      // ± ops/s
-    private Map<String, Double> scorePercentiles; // z.B. T50, T90
+  private String name;
+  private int bitSize;
+  private double score; // ops/s
+  private double scoreError; // ± ops/s
+  private Map<String, Double> scorePercentiles;
 
-    // Abgeleitete Metriken
-    private double opsPerMs;
-    private double latencyUs;
+  // Abgeleitete Metriken
+  private double opsPerMs;
+  private double latencyUs;
 
-    // Neue Metriken
-    private double heapMb;     // Heap-Verbrauch in MB
-    private double cpuLoad;    // CPU-Auslastung in Prozent
+  // Neue Metriken
+  private double heapMb; // Heap-Verbrauch in MB
+  private double cpuLoad; // CPU-Auslastung in Prozent
 
-    public BenchmarkResult() {
-        scorePercentiles = new LinkedHashMap<>();
-    }
+  public BenchmarkResult() {
+    scorePercentiles = new LinkedHashMap<>();
+  }
 
-    // Getter und Setter
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+  // Getter und Setter
+  public String getName() {
+    return name;
+  }
 
-    public int getBitSize() { return bitSize; }
-    public void setBitSize(int bitSize) { this.bitSize = bitSize; }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public double getScore() { return score; }
-    public void setScore(double score) { this.score = score; }
+  public int getBitSize() {
+    return bitSize;
+  }
 
-    public double getScoreError() { return scoreError; }
-    public void setScoreError(double scoreError) { this.scoreError = scoreError; }
+  public void setBitSize(int bitSize) {
+    this.bitSize = bitSize;
+  }
 
-    public Map<String, Double> getScorePercentiles() { return scorePercentiles; }
-    public void setScorePercentiles(Map<String, Double> scorePercentiles) {
-        this.scorePercentiles = scorePercentiles;
-    }
+  public double getScore() {
+    return score;
+  }
 
-    public double getOpsPerMs() { return opsPerMs; }
-    public void setOpsPerMs(double opsPerMs) { this.opsPerMs = opsPerMs; }
+  public void setScore(double score) {
+    this.score = score;
+  }
 
-    public double getLatencyUs() { return latencyUs; }
-    public void setLatencyUs(double latencyUs) { this.latencyUs = latencyUs; }
+  public double getScoreError() {
+    return scoreError;
+  }
 
-    public double getHeapMb() { return heapMb; }
-    public void setHeapMb(double heapMb) { this.heapMb = heapMb; }
+  public void setScoreError(double scoreError) {
+    this.scoreError = scoreError;
+  }
 
-    public double getCpuLoad() { return cpuLoad; }
-    public void setCpuLoad(double cpuLoad) { this.cpuLoad = cpuLoad; }
+  public Map<String, Double> getScorePercentiles() {
+    return scorePercentiles;
+  }
+
+  public void setScorePercentiles(Map<String, Double> scorePercentiles) {
+    this.scorePercentiles = scorePercentiles;
+  }
+
+  public double getOpsPerMs() {
+    return opsPerMs;
+  }
+
+  public void setOpsPerMs(double opsPerMs) {
+    this.opsPerMs = opsPerMs;
+  }
+
+  public double getLatencyUs() {
+    return latencyUs;
+  }
+
+  public void setLatencyUs(double latencyUs) {
+    this.latencyUs = latencyUs;
+  }
+
+  public double getHeapMb() {
+    return heapMb;
+  }
+
+  public void setHeapMb(double heapMb) {
+    this.heapMb = heapMb;
+  }
+
+  public double getCpuLoad() {
+    return cpuLoad;
+  }
+
+  public void setCpuLoad(double cpuLoad) {
+    this.cpuLoad = cpuLoad;
+  }
 }
